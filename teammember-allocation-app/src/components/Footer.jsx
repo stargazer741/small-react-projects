0f358c1d787152a834e436d168f23dd68729fa74
stargazer741 <102627303+stargazer741@users.noreply.github.com>
2023-02-01 12:35:55 +0300

    saving selected teams and members to the localstorage with useEffect

--- teammember-allocation-app/src/components/Footer.jsx
+++ teammember-allocation-app/src/components/Footer.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
+let today = new Date();
+
 const Footer = () => {
   return (
-    <footer>
-      <h1>Footer</h1>
+    <footer className="container">
+      <div className="row justify-content-center mt-3 mb-4">
+        <div className="col-8">
+          <h5>Team Member Allocation App {today.getFullYear()}</h5>
+        </div>
+      </div>
     </footer>
   );
 };
